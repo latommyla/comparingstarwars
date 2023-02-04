58db99156a499dbb29fda3aa59d78d930d28eaef
Tommy La <tommyl.dmd@gmail.com>
2023-02-04 11:55:25 -0800

    all packages and components added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './app.css';
+
+const App = () =>{
+    return (
+        <h1 className="text-4xl">
+            Hello world! I am using React
+        </h1>
+    )
+}
+
+export default App;
